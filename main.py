d3face2c6ca806a0d70cea1df563ba8dc2494f5d
techy4shri <sushri4tech@gmail.com>
2024-12-20 21:43:01 +0530

    started with main.py

---
+++ main.py
@@ -0,0 +1,51 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QPushButton, QFileDialog
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        layout = QVBoxLayout()
+
+        self.appInput = QLineEdit(self)
+        self.appInput.setPlaceholderText('Application launching')
+        layout.addWidget(self.appInput)
+
+        self.startTimeInput = QLineEdit(self)
+        self.startTimeInput.setPlaceholderText('Start time')
+        layout.addWidget(self.startTimeInput)
+
+        self.stopTimeInput = QLineEdit(self)
+        self.stopTimeInput.setPlaceholderText('Stop time')
+        layout.addWidget(self.stopTimeInput)
+
+        self.browseButton = QPushButton('Browse', self)
+        self.browseButton.clicked.connect(self.browseFile)
+        layout.addWidget(self.browseButton)
+
+        self.runButton = QPushButton('Run', self)
+        self.runButton.clicked.connect(self.runApplication)
+        layout.addWidget(self.runButton)
+
+        self.setLayout(layout)
+        self.setWindowTitle('OpenModelica GUI')
+        self.show()
+
+    def browseFile(self):
+        fileName, _ = QFileDialog.getOpenFileName(self, 'Open File', '', 'Executable Files (*.exe)')
+        if fileName:
+            self.appInput.setText(fileName)
+
+    def runApplication(self):
+        appPath = self.appInput.text()
+        startTime = self.startTimeInput.text()
+        stopTime = self.stopTimeInput.text()
+        # Add code to execute the application with start and stop times
+        print(f'Running {appPath} from {startTime} to {stopTime}')
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = MainWindow()
+    sys.exit(app.exec_())
