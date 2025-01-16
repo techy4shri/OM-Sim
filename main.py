f82496243979a71960142b1882beb417ccc82990
techy4shri <sushri4tech@gmail.com>
2025-01-17 01:20:25 +0530

    added requirements.txt modified the error handling and function validation in main.py

--- main.py
+++ main.py
@@ -11,86 +11,84 @@ class MainWindow(QWidget):
         layout = QVBoxLayout()
         self.setWindowTitle("OpenModelica Simulation App")
 
+        #executable path field
+        #this field will be filled with path of the executable file upon browsing
         self.appInput = QLineEdit(self)
-        self.appInput.setPlaceholderText('Search executables to launch')
+        self.appInput.setPlaceholderText('Browse executables to launch')
         layout.addWidget(self.appInput)
+
+        #browse button to select the executable file
+        #only searches for .exe files, no error handling needed therefore
+        self.browseButton = QPushButton('Browse', self)
+        self.browseButton.clicked.connect(self.browseFile)
+        layout.addWidget(self.browseButton)
         
+        #start time of the execution of executable to launch
 
         self.startTimeInput = QLineEdit(self)
         self.startTimeInput.setPlaceholderText('Start time of execution')
         layout.addWidget(self.startTimeInput)
         
-
+        #stop time of the execution of executable to launch
         self.stopTimeInput = QLineEdit(self)
         self.stopTimeInput.setPlaceholderText('Stop time of execution')
         layout.addWidget(self.stopTimeInput)
-        self.stopTimeInput.size()
-
-        self.browseButton = QPushButton('Browse', self)
-        self.browseButton.clicked.connect(self.browseFile)
-        layout.addWidget(self.browseButton)
-        self.browseButton.size()
+        
+        
         self.runButton = QPushButton('Run', self)
         self.runButton.clicked.connect(self.runApplication)
         layout.addWidget(self.runButton)
 
-        #need to put this in a func
-        self.setFixedSize(500, 250) 
+        #need to put this in a func 
         self.setMinimumSize(400, 200)
         self.setMaximumSize(1200, 600)
-
         self.setLayout(layout)
-        self.setWindowTitle('OpenModelica GUI')
         self.show()
-
-        
-
-        
 
     #layout proper
     layout = QVBoxLayout()
 
-    
-
-    def browseFile(self): #needs to be placed properly
+    def browseFile(self): 
         fileName, _ = QFileDialog.getOpenFileName(self, 'Open File', '', 'Executable Files (*.exe)')
         if fileName:
          self.appInput.setText(fileName)
-        
-    
 
-    
 
     def runApplication(self):
         appPath = self.appInput.text()
         startTime = self.startTimeInput.text()
         stopTime = self.stopTimeInput.text()
-        if appPath and startTime and stopTime:
+        
+        #error handling and validation of inputs
+        if not appPath:
+            QMessageBox.critical(self, "Inout Error", "Please select an executable file.")
+            return
             
-            try:
-                startTimeInput = int(startTime)
-                stopTimeInput = int(stopTime)
-                if not (0<= startTimeInput < stopTimeInput < 5):
-                    raise ValueError('oopsie daisy')
-            except ValueError as e:
-                QMessageBox.critical(self,"Input error", str(e))
-                return
+        try:
+            startTime = int(startTime)
+            stopTime = int(stopTime)
+            if not (0<= startTime < stopTime < 5):
+                raise ValueError('Oopsie daisy, start time must be less than stop time, and both should be in the range of [0,5]')
+        except ValueError as e:
+            QMessageBox.critical(self,"Input error", str(e))
+            return
 
-            try:
-                result =subprocess.run([  #check the subprocess of open-modelica
-                        appPath, 
-                        f"-override=startTime={startTime}, 
-                         stopTime={stopTime}"], #adding time contraints as mentioned in docs
-                        capture_output=True, 
-                        text=True,
-                        check=True
-                        )
-                QMessageBox.information(self, f"Successful execution!\n\nOutput: \n{result.stdoutput}")
-            except subprocess.CalledProcessError as e:
-                QMessageBox.critical(self, "Execution error", f"Error during simulation:\n{e.stderr}")
-
-            except Exception as e:
-                print(f'Failed to run application: {e}')
+        #running the executable with -override arg
+        #this argument will override the current XML of the TwoConnectedTanks.exe file while running
+        #allows the user to run the executable as desired
+        try:
+            result =subprocess.run([  #check the subprocess of open-modelica
+                appPath, 
+                f"-override=startTime={startTime},stopTime={stopTime}"], #adding time contraints as mentioned in docs
+                capture_output=True, 
+                text=True,
+                check=True
+            )
+            QMessageBox.information(self, "SUCCESS", f"Successful execution!\n\nOutput: \n{result.stdout}")
+        except subprocess.CalledProcessError as e:
+            QMessageBox.critical(self, "Execution error", f"Error during simulation:\n{e.stderr}")
+        except Exception as e:
+            QMessageBox.critical(self, "Unexpected Error" , f'Failed to run application: {str(e)}')
         else:
             print('Please provide all inputs: application path, start time, and stop time')
 
@@ -98,4 +96,4 @@ if __name__ == '__main__':
     app = QApplication([])
     mainWindow = MainWindow()
     sys.exit(app.exec())
- #modify the run and start to be included as buttons
+ 
